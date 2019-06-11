5bdb67a0ab72b63ed9601a0ce84588560c7cceea
Alexander Maricich <amaricich@skylinenet.net>
2019-06-11 12:07:46 -0400

    removed comment

--- clj-kj.sh
+++ clj-kj.sh
@@ -9,5 +9,4 @@
 # joker:   <filename>:<line>:<column>: <issue type>: <message>
 # kakoune: <filename>:<line>:<column>: <error|warn>: <message>
 
-# joker --lint $@ 2>&1 | sed 's/ Read error:/ error:/g; s/ Parse error:/ error:/g; s/ Parse warning:/ warn:/g; s/ Exception:/ error:/g'
 joker --lint $@ 2>&1 | awk -F ':' '$4 = ($4 ~ /warn/ ? "warn" : "error") { printf "%s:%s:%s: %s: %s\n", $1, $2, $3, $4, $5 }'
